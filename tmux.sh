a0e206477dc9c9d290ef98b3b457b86570bf683a
banbxio <a31521424@163.com>
2024-03-28 22:59:18 +0800

    optimize tmux

--- tmux.sh
+++ tmux.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-git clone https://github.com/gpakosz/.tmux.git "/path/to/oh-my-tmux"
-mkdir -p "~/.config/tmux"
-ln -s "/path/to/oh-my-tmux/.tmux.conf" "~/.config/tmux/tmux.conf"
-cp "/path/to/oh-my-tmux/.tmux.conf.local" "~/.config/tmux/tmux.conf.local"
+
+cd $HOME
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
